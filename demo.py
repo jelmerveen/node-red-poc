972822612aad3e172f4524666d834fc9cabab5b0
Jelmer Veen <j.p.veen@rug.nl>
2022-05-19 16:58:06 +0200

    websockets & watchers

--- demo.py
+++ demo.py
@@ -1,1 +1,19 @@
-print("Message from Python!")
+import time
+
+with open('log.txt', 'w') as f:
+    f.write("Message from Python!")
+
+time.sleep(2)
+
+with open('log.txt', 'w') as f:
+    f.write("Second line")
+
+time.sleep(2)
+
+with open('log.txt', 'w') as f:
+    f.write("Third line")
+
+time.sleep(2)
+
+with open('log.txt', 'w') as f:
+    f.write("The end.")
